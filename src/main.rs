46bfee49cfe6e70224c3c3324306f925d049e3f9
lthoerner <lowellashton@gmail.com>
2025-10-21 16:34:07 -0700

    use dynamic map for locale paths

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
 use std::path::{Path, PathBuf};
@@ -9,46 +10,9 @@ use serde::{Deserialize, Serialize};
 use soft_canonicalize::soft_canonicalize;
 
 #[derive(Serialize, Deserialize)]
-struct LocaleManifest(LocaleFileLocations);
-
-#[derive(Serialize, Deserialize, Default)]
-#[serde(rename = "locale_files")]
-struct LocaleFileLocations {
-    ar: Option<String>,
-    bg: Option<String>,
-    cs: Option<String>,
-    da: Option<String>,
-    de: Option<String>,
-    el: Option<String>,
-    en_gb: Option<String>,
-    en_us: Option<String>,
-    es: Option<String>,
-    es_419: Option<String>,
-    et: Option<String>,
-    fi: Option<String>,
-    fr: Option<String>,
-    hu: Option<String>,
-    id: Option<String>,
-    it: Option<String>,
-    ja: Option<String>,
-    ko: Option<String>,
-    lt: Option<String>,
-    lv: Option<String>,
-    nb: Option<String>,
-    nl: Option<String>,
-    pl: Option<String>,
-    pt_br: Option<String>,
-    pt_pt: Option<String>,
-    ro: Option<String>,
-    ru: Option<String>,
-    sk: Option<String>,
-    sl: Option<String>,
-    sv: Option<String>,
-    tr: Option<String>,
-    uk: Option<String>,
-    zh: Option<String>,
-    zh_hans: Option<String>,
-    zh_hant: Option<String>,
+struct LocaleManifest {
+    source_locale_path: String,
+    locale_paths: HashMap<String, String>,
 }
 
 fn main() {
@@ -87,10 +51,10 @@ fn main() {
                 break english_locale_path;
             };
 
-            LocaleManifest(LocaleFileLocations {
-                en_us: Some(english_locale_path.to_string_lossy().to_string()),
-                ..Default::default()
-            })
+            LocaleManifest {
+                source_locale_path: english_locale_path.to_string_lossy().to_string(),
+                locale_paths: HashMap::new(),
+            }
         }
     };
 
@@ -222,6 +186,8 @@ fn main() {
     };
 
     eprintln!("Output saved to {}.", output_locale_path.to_string_lossy());
+
+    write_manifest(locale_manifest_data);
 }
 
 fn confirm_prompt(prompt_text: &str) -> bool {
